f824438e3a468e7d501bf30064baf86ee799713b
Pramendra Sharma <sharmarahul111.psr@gmail.com>
2026-03-01 17:42:24 +0545

    A movable player within screen

--- code/main.py
+++ code/main.py
@@ -1,14 +1,25 @@
 from settings import *
 from custom_timer import Timer
-
+from sprites import Player
 class Game():
 	def __init__(self):
 		init_window(WINDOW_WIDTH, WINDOW_HEIGHT, "Space Shooter")
+		self.import_assets()
+
+		self.player = Player(self.assets["player"], Vector2(WINDOW_WIDTH/2, WINDOW_HEIGHT/2))
+
+	def import_assets(self):
+		self.assets = {
+			"player": load_texture(join("images", "spaceship.png"))
+		}
 
 	def run(self):
 		while not window_should_close():
+			dt = get_frame_time()
+			self.player.update(dt)
 			begin_drawing()
-
+			clear_background(BG_COLOR)
+			self.player.draw()
 			end_drawing()
 		close_window()
 
--- code/sprites.py
+++ code/sprites.py
@@ -1,16 +1,36 @@
 from settings import *
 
 class Sprite():
-	def __init__(self, texture, pos, direction):
+	def __init__(self, texture, pos, speed, direction):
 		self.pos = pos
 		self.texture = texture
+		self.speed = speed
 		self.direction = direction
-	def update(self):
+		self.size = Vector2(texture.width, texture.height)
+	def move(self, dt):
+		self.pos.x += self.direction.x * self.speed * dt
+		self.pos.y += self.direction.y * self.speed * dt
+	def update(self, dt):
 		pass
 	def draw(self):
 		draw_texture_v(self.texture, self.pos, WHITE)
 
 class Player(Sprite):
 	def __init__(self, texture, pos):
-		super().__init__(pos, texture, PLAYER_SPEED, Vector2())
+		super().__init__(texture ,pos, PLAYER_SPEED, Vector2())
+
+	def input(self):
+		self.direction.x = int(is_key_down(KEY_RIGHT))-int(is_key_down(KEY_LEFT))
+		self.direction.y = int(is_key_down(KEY_DOWN))-int(is_key_down(KEY_UP))
+		self.direction = vector2_normalize(self.direction)
+
+		if is_key_pressed(KEY_SPACE):
+			pass
+	def constraint(self):
+		self.pos.x = max(0, min(self.pos.x, WINDOW_WIDTH-self.size.x))
+		self.pos.y = max(0, min(self.pos.y, WINDOW_HEIGHT-self.size.y))
+	def update(self, dt):
+		self.input()
+		self.move(dt)
+		self.constraint()
 
