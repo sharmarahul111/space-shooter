e3ad31537ce257b21ba96e5da3a75e5134f61472
Pramendra Sharma <sharmarahul111.psr@gmail.com>
2026-03-01 23:56:52 +0545

    Audio, Music, Space

--- code/main.py
+++ code/main.py
@@ -6,12 +6,15 @@ from sprites import Player, Laser, Meteor, ExplosionAnimation
 class Game():
 	def __init__(self):
 		init_window(WINDOW_WIDTH, WINDOW_HEIGHT, "Space Shooter")
+		init_audio_device()
 		set_target_fps(60)
 		self.import_assets()
 		self.lasers, self.meteors, self.explosions = [], [], []
 		self.meteor_timer = Timer(METEOR_TIMER_DURATION, True, True, self.create_meteor)
 		self.player = Player(self.assets["player"], Vector2(
-			WINDOW_WIDTH/2, WINDOW_HEIGHT/2), self.shoot_laser)
+			WINDOW_WIDTH/2, WINDOW_HEIGHT/2), self.shoot_laser
+		)
+		play_music_stream(self.audio["music"])
 
 	def import_assets(self):
 		self.assets = {
@@ -22,6 +25,11 @@ class Game():
 			"explosion": [load_texture(join("images", "explosion", f"{i}.png")) for i in range(1, 29)],
 			"font": load_font_ex(join("font", "Stormfaze.otf"), FONT_SIZE, ffi.NULL, 0)
 		}
+		self.audio = {
+			"laser": load_sound(join("audio", "laser.wav")),
+			"explosion": load_sound(join("audio", "explosion.wav")),
+			"music": load_music_stream(join("audio", "music.wav"))
+		}
 		self.star_data = [
 			(
 				Vector2(randint(0, WINDOW_WIDTH),
@@ -29,20 +37,25 @@ class Game():
 				uniform(.5, 1.6)  # size
 			) for i in range(40)
 		]
+	
 	def shoot_laser(self, pos):
 		self.lasers.append(Laser(self.assets["laser"], pos))
+		play_sound(self.audio["laser"])
+
 	def draw_stars(self):
 		for star in self.star_data:
 			draw_texture_ex(self.assets["star"], star[0], 0, star[1], WHITE)
+
 	def discard_sprites(self):
 		self.lasers = [laser for laser in self.lasers if not laser.discard]
 		self.meteors = [meteor for meteor in self.meteors if not meteor.discard]
 		self.explosions = [explosion for explosion in self.explosions if not explosion.discard]
+
 	def draw_score(self):
 		score = int(get_time())
 		text = measure_text_ex(self.assets["font"], str(score), FONT_SIZE, 0)
 		draw_text_ex(self.assets["font"], str(score), Vector2(WINDOW_WIDTH/2 - text.x/2, 100), FONT_SIZE, 0, WHITE)
-		pass
+
 	def create_meteor(self):
 		self.meteors.append(Meteor(self.assets["meteor"]))
 	def check_collision(self):
@@ -57,6 +70,7 @@ class Game():
 					laser.discard, meteor.discard = True, True
 					pos = Vector2(laser.pos.x-laser.size.x/2, laser.pos.y)
 					self.explosions.append(ExplosionAnimation(pos, self.assets["explosion"]))
+					play_sound(self.audio["explosion"])
 		#player and meteor
 		for meteor in self.meteors:
 			if check_collision_circles(
@@ -64,8 +78,8 @@ class Game():
 				self.player.collision_radius,
 				meteor.get_center(), meteor.collision_radius
 			):
-				pass
-				# close_window()
+				close_window()
+
 	def update(self):
 		dt = get_frame_time()
 		self.meteor_timer.update()
@@ -74,6 +88,8 @@ class Game():
 		for sprite in self.lasers + self.meteors + self.explosions:
 			sprite.update(dt)
 		self.check_collision()
+		update_music_stream(self.audio["music"])
+
 	def draw(self):
 		begin_drawing()
 		clear_background(BG_COLOR)
@@ -83,11 +99,13 @@ class Game():
 		for sprite in self.lasers + self.meteors + self.explosions:
 			sprite.draw()
 		end_drawing()
+		
 	def run(self):
 		while not window_should_close():
 			self.update()
 			self.draw()
-
+		unload_music_stream(self.audio["music"])
+		close_audio_device()
 		close_window()
 
 
--- code/sprites.py
+++ code/sprites.py
@@ -12,14 +12,18 @@ class Sprite():
 		
 	def get_center(self):
 		return Vector2(self.pos.x + self.size.x/2, self.pos.y + self.size.y/2)
+
 	def move(self, dt):
 		self.pos.x += self.direction.x * self.speed * dt
 		self.pos.y += self.direction.y * self.speed * dt
+
 	def check_discard(self):
 		self.discard = not -300 < self.pos.y < WINDOW_HEIGHT + 300
+
 	def update(self, dt):
 		self.move(dt)
 		self.check_discard()
+
 	def draw(self):
 		draw_texture_v(self.texture, self.pos, WHITE)
 
@@ -35,16 +39,20 @@ class Player(Sprite):
 
 		if is_key_pressed(KEY_SPACE):
 			self.shoot_laser(Vector2(self.pos.x + self.size.x/2, self.pos.y-60))
+
 	def constraint(self):
 		self.pos.x = max(0, min(self.pos.x, WINDOW_WIDTH-self.size.x))
 		self.pos.y = max(0, min(self.pos.y, WINDOW_HEIGHT-self.size.y))
+
 	def update(self, dt):
 		self.input()
 		self.move(dt)
 		self.constraint()
+
 class Laser(Sprite):
 	def __init__(self, texture, pos):
 		super().__init__(texture, pos, LASER_SPEED, Vector2(0, -1))
+
 	def get_rect(self):
 		return Rectangle(self.pos.x, self.pos.y, self.size.x, self.size.y)
 
@@ -60,8 +68,10 @@ class Meteor(Sprite):
 	def update(self, dt):
 		super().update(dt)
 		self.rotation += 50 * dt
+
 	def get_center(self):
 		return self.pos
+
 	def draw(self):
 		target_rect = Rectangle(self.pos.x, self.pos.y, self.size.x, self.size.y)
 		origin = Vector2(self.size.x/2, self.size.y/2)
@@ -74,11 +84,12 @@ class ExplosionAnimation():
 		self.pos = Vector2(pos.x - self.size.x/2, pos.y-self.size.y/2)
 		self.index = 0
 		self.discard = False
+
 	def update(self, dt):
-		print(len(self.textures), self.index)
 		if self.index < len(self.textures) - 1:
 			self.index += 20 * dt
 		else:
 			self.discard = True
+			
 	def draw(self):
 		draw_texture_v(self.textures[int(self.index)], self.pos, WHITE)
