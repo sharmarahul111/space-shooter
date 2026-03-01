47254f6513f32f53291e2aae4743a7026202f846
Pramendra Sharma <sharmarahul111.psr@gmail.com>
2026-03-01 16:58:46 +0545

    Game class

--- code/main.py
+++ code/main.py
@@ -1,2 +1,17 @@
 from settings import *
 from custom_timer import Timer
+
+class Game():
+	def __init__(self):
+		init_window(WINDOW_WIDTH, WINDOW_HEIGHT, "Space Shooter")
+
+	def run(self):
+		while not window_should_close():
+			begin_drawing()
+
+			end_drawing()
+		close_window()
+
+if __name__ == "__main__":
+	game = Game()
+	game.run()
