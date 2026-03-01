c7aaf976dda5b233fe3d05c5d8e0404a935a76df
Pramendra Sharma <sharmarahul111.psr@gmail.com>
2026-03-01 18:48:43 +0545

    Refactor update and draw

--- code/main.py
+++ code/main.py
@@ -1,39 +1,52 @@
 from settings import *
 from custom_timer import Timer
 from sprites import Player
+
+
 class Game():
-	def __init__(self):
-		init_window(WINDOW_WIDTH, WINDOW_HEIGHT, "Space Shooter")
-		self.import_assets()
+    def __init__(self):
+        init_window(WINDOW_WIDTH, WINDOW_HEIGHT, "Space Shooter")
+        self.import_assets()
 
-		self.player = Player(self.assets["player"], Vector2(WINDOW_WIDTH/2, WINDOW_HEIGHT/2))
+        self.player = Player(self.assets["player"], Vector2(
+            WINDOW_WIDTH/2, WINDOW_HEIGHT/2))
 
-	def import_assets(self):
-		self.assets = {
-			"player": load_texture(join("images", "spaceship.png")),
-			"star": load_texture(join("images", "star.png"))
-		}
-		self.star_data = [
-			(
-			Vector2(randint(0, WINDOW_WIDTH), randint(0, WINDOW_HEIGHT)), #pos
-			uniform(.5, 1.6) #size
-			) for i in range(40)
-		]
-	def draw_stars(self):
-		for star in self.star_data:
-			draw_texture_ex(self.assets['star'], star[0], 0, star[1], WHITE)
+    def import_assets(self):
+        self.assets = {
+            "player": load_texture(join("images", "spaceship.png")),
+            "star": load_texture(join("images", "star.png"))
+        }
+        self.star_data = [
+            (
+                Vector2(randint(0, WINDOW_WIDTH),
+                        randint(0, WINDOW_HEIGHT)),  # pos
+                uniform(.5, 1.6)  # size
+            ) for i in range(40)
+        ]
 
-	def run(self):
-		while not window_should_close():
-			dt = get_frame_time()
-			self.player.update(dt)
-			begin_drawing()
-			clear_background(BG_COLOR)
-			self.draw_stars()
-			self.player.draw()
-			end_drawing()
-		close_window()
+    def draw_stars(self):
+        for star in self.star_data:
+            draw_texture_ex(self.assets['star'], star[0], 0, star[1], WHITE)
+
+    def update(self):
+        dt = get_frame_time()
+        self.player.update(dt)
+
+    def draw(self):
+        begin_drawing()
+        clear_background(BG_COLOR)
+        self.draw_stars()
+        self.player.draw()
+        end_drawing()
+
+    def run(self):
+        while not window_should_close():
+            self.update()
+            self.draw()
+
+        close_window()
+
 
 if __name__ == "__main__":
-	game = Game()
-	game.run()
+    game = Game()
+    game.run()
